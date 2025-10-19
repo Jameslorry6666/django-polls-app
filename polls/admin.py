5186afff9c7c20443df382c6e3a1ff1de1138450
James <oluwasegunjamesilori@gmail.com>
2025-10-20 02:39:24 +0700

    Add database models:Question and Choice with Initial migration

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Question, Choice  # Fix the space after "models"
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
+
+# polls/admin.py
+
+
+from django.contrib import admin
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
